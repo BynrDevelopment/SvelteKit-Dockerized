651e63caa0c1efa713b54b820f67940d6b656217
Leon <92170476+L50N@users.noreply.github.com>
2024-10-30 22:05:10 +0100

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+console.log('Entwicklung durch BynexIT® 2024');
+console.log('Addition: Ein offizielles BynexIT® Projekt.');
+console.log('Visit https://bynex.me/ to see more!');
